381b2fe9aff07d9598be866852232dcdd2c84e21
Daniel Ribeiro <danrbr@gmail.com>
2014-01-31 19:41:33 -0800

    update to keymap

--- user.keymap.clj
+++ user.keymap.clj
@@ -1,32 +1,10 @@
-
 {:+ {:app {}
 
     :editor {"alt-w" [:editor.watch.watch-selection]
-             "pmeta-/"  [:toggle-comment-selection]
-              "alt-shift-w" [:editor.watch.unwatch]
+             "pmeta-/" [:toggle-comment-selection]
+             "alt-shift-w" [:editor.watch.unwatch]
+             "pmeta-i" [:clear-console]
               }
-    :editor.clj {"pmeta-e" [(:eval.custom "(do (require 'kibit.check)
-                                           (if-let [better (kibit.check/check-expr '__SELECTION__)]
-                                             (:alt better)
-                                             '__SELECTION__
-                                           ))"
-                                           {:result-type :replace})]
-
-                  "pmeta-i" [(:eval.custom "(str (type '__SELECTION__ ) \" \" '__SELECTION__ )"
-                                           {:result-type :replace})]
-     
-                  "pmeta-p" [(:eval.custom "*ns*"
-                                           {:result-type :inline})]}     
-
-;;     :editor.clj {"pmeta-p" [(:eval.custom " (let [commands (clojure.string/split-lines '__SELECTION*__ )
-;;                                                   output (map (fn [command]
-;;                                                     (->> (clojure.string/split command #\"\\s+\") (apply clojure.java.shell/sh) (:out))) commands)]
-;;                                                   (clojure.string/join \"\\n\" output ))                                          "
-;;                                            {:result-type :replace})]}
-
-
-
-
 
       :tabs {
       "ctrl-pageup" [:tabs.prev]
